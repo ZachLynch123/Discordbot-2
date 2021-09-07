a0bef6b9f6796d0ad3377341f17514d0dd4106e3
Zach Lynch <zachary.lynch1337@gmail.com>
2021-09-07 16:50:32 -0700

    changing to python interface

---
+++ keys/keys.js
@@ -0,0 +1,6 @@
+const clientId = 550110824014938298;
+const clientSecret = 'y2IOuUjjKr5RFIsMcG3qoG-2Pw8Lk5oA';
+const clientToken = 'NTUwMTEwODI0MDE0OTM4Mjk4.XHXYoQ.l40G765dS61ZXnQk30lXoUfIc5E';
+
+
+export default (clientId, clientSecret, clientToken);
